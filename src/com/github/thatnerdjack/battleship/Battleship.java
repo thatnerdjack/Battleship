ddc75b3af5da5bc02a6ae473b3b885a1e90c978e
thatnerdjack <jackjyro@gmail.com>
2014-12-02 12:37:02 -0500

    Initial commit

---
+++ src/com/github/thatnerdjack/battleship/Battleship.java
@@ -0,0 +1,12 @@
+package com.github.thatnerdjack.battleship;
+
+/**
+ * Created by block7 on 12/2/14.
+ */
+public class Battleship {
+
+    public static void main(String args[]) {
+        //code here
+    }
+
+}
